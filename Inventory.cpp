75e4335ee946261ac1842551cd390181bd2c2ad2
Griff <grifsf@gmail.com>
2020-02-12 11:18:02 -0800

    adds comments to functions of Inventory

--- Inventory.cpp
+++ Inventory.cpp
@@ -4,19 +4,30 @@
 
 using std::string;
 using std::ostream;
-  
+/*
+ * Constructor for inventory
+ * @param name: string that is name of item
+ * @param price: float that is the price of said item
+ * @param count: how many of item is in stock
+ */
 Inventory::Inventory(string name, float price, int count)
 {
   m_name = name;
   m_price = price;
   m_in_stock = count;
 }
-
+/*
+ *Void function that sells stock
+ */
 void Inventory::sell()
 {
   m_in_stock--;
 }
-
+/*
+ *Creates and ostream with values taken from the item
+ *@param stream: ostream passes by reference
+ *@param item: Inventory item passed by reference
+ */
 ostream& operator<<(ostream& stream, const Inventory& item)
 {
   stream << item.m_name 
